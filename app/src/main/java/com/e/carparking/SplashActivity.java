c1eb3c29059ab88702cfce83d002c63311fe56ba
krmickey <krmickey1@gmail.com>
2019-12-10 21:57:23 +0530

    Design SignIn page /activity_signin.xml

--- app/src/main/java/com/e/carparking/SplashActivity.java
+++ app/src/main/java/com/e/carparking/SplashActivity.java
@@ -2,7 +2,12 @@ package com.e.carparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.e.carparking.activities.SigninActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/e/carparking/activities/SigninActivity.java
+++ app/src/main/java/com/e/carparking/activities/SigninActivity.java
@@ -3,6 +3,8 @@ package com.e.carparking.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.e.carparking.R;
 
@@ -11,7 +13,12 @@ public class SigninActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Sign In");
         setContentView(R.layout.activity_signin);
+        TextView uName = findViewById(R.id.userName);
+        TextView uMailId = findViewById(R.id.userMailID);
+        TextView uPhoneNo = findViewById(R.id.userPhoneNo);
+        Button signUpButton = findViewById(R.id.signUpButton);
 
     }
 }
